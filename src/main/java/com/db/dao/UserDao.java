83f02e98aa2fec422b996a129dd590cc6236ab71
yjyj1023 <lyj19961023@gmail.com>
2022-11-06 22:41:29 +0900

    DaoFactory 까지 추가(수정)

--- src/main/java/com/db/dao/UserDao.java
+++ src/main/java/com/db/dao/UserDao.java
@@ -8,7 +8,7 @@ public class UserDao {
     private ConnectionMaker connectionMaker;
 
     public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = new AWSConnectionMaker();
+        this.connectionMaker = connectionMaker;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
